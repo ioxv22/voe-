4da558c60ce85dfada483f510155a5385517849e
iivoz <iivoz@telegram.com>
2026-04-17 11:10:24 +0400

    fix: navbar syntax error and css import order for vercel build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -225,6 +225,7 @@ export default function Navbar() {
                         </div>
                     </>
                 )}
+                </div>
             </div>
         ) : (
             <button 
